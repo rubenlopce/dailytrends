143169771830ffea31e06caec87cffe64fe4597e
rubenlopce <rubenlopce@gmail.com>
2019-10-17 17:59:14 +0200

    Feed controller.

---
+++ config/controllers/feed.php
@@ -0,0 +1,23 @@
+<?php
+
+    class FeedController{
+
+        public function createFeed(){
+
+        }
+
+        public function showFeed(){
+
+        }
+
+        public function updateFeed(){
+
+        }
+
+        public function deleteFeed(){
+
+        }
+
+    }
+
+?>
