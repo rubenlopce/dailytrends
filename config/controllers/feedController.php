1e19d5f64c62624f208c9f0bb2ca7fda22e3f04e
rubenlopce <rubenlopce@gmail.com>
2019-10-20 12:35:28 +0200

    Changed the name of the controller file and added logic to the Update and Delete functions.

---
+++ config/controllers/feedController.php
@@ -0,0 +1,45 @@
+<?php
+
+    class feedController{
+
+        public function createFeed(){
+
+        }
+
+        public function showFeed(){
+
+        }
+
+        public function updateFeed($title,$image,$publisher,$body,$source,$position,$feedArray){
+
+            $feedArray[$position]->setTitle($title);
+            $feedArray[$position]->setImage($image);
+            $feedArray[$position]->setPublisher($publisher);
+            $feedArray[$position]->setBody($body);
+            $feedArray[$position]->setSource($source);
+
+            return $feedArray;
+
+        }
+
+        public function deleteFeed($position,$feedArray){
+
+            array_splice($feedArray,1,$position);
+
+            return $feedArray;
+
+        }
+
+        public function showAll($feedArray){
+
+            foreach ($feedArray as $key => $feed) {
+                
+                require DIR_VIEWS.'feed/list.php';
+            
+            }
+
+        }
+
+    }
+
+?>
