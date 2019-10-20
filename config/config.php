fca289fb79fe21e8b88ae4b76d088d260816055a
rubenlopce <rubenlopce@gmail.com>
2019-10-20 16:57:37 +0200

    Added path to the folder 'Widgets'.

--- config/config.php
+++ config/config.php
@@ -2,6 +2,7 @@
 
 define('DIR_BASE',      dirname( dirname( __FILE__ ) ) . '/');
 define('DIR_BLOCKS',    DIR_BASE . 'app/blocks/');
+define('DIR_WIDGETS',    DIR_BASE . 'app/widgets/');
 define('DIR_VIEWS',     DIR_BASE . 'config/views/');
 define('DIR_CTLS',      DIR_BASE . 'config/controllers/');
 define('DIR_MDLS',      DIR_BASE . 'config/models/');
