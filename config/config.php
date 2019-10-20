2bdf69f9050a6f2b80f90f428ba371fc39accefc
rubenlopce <rubenlopce@gmail.com>
2019-10-20 12:45:10 +0200

    Created a file to handle the paths of the files.

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('DIR_BASE',      dirname( dirname( __FILE__ ) ) . '/');
+define('DIR_BLOCKS',    DIR_BASE . 'app/blocks/');
+define('DIR_VIEWS',     DIR_BASE . 'config/views/');
+define('DIR_CTLS',      DIR_BASE . 'config/controllers/');
+define('DIR_MDLS',      DIR_BASE . 'config/models/');
+define('HEADER',   DIR_BLOCKS . 'header.php');
+define('FOOTER',   DIR_BLOCKS . 'footer.php');
+
+?>
