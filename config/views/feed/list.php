31ba5076a982a77ae8bff2ecb42bc9b53c1f6cc8
rubenlopce <rubenlopce@gmail.com>
2019-10-20 16:56:39 +0200

    Created files to handle Update and Delete of feeds, and a form to edit and delete the chosen feed.

--- config/views/feed/list.php
+++ config/views/feed/list.php
@@ -1,9 +1,10 @@
-<div data-number='<?= $key ?>'>
+<div>
 
     <img src="<?=$feed->getImage();?>" alt="">
     <h1><?=$feed->getTitle();?></h1>
     <p><?=$feed->getPublisher();?></p>
     <p><?=$feed->getBody();?></p>
     <small><?=$feed->getSource();?></small>
+    <button class='details' value='<?= $key ?>'>Detalles</button>
 
 </div>
---
+++ config/views/feed/update.php
@@ -0,0 +1,28 @@
+<?php
+
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    require_once '../../config.php';
+    require_once DIR_MDLS.'feed.php';
+    require_once DIR_CTLS.'feedController.php';
+
+    session_start();
+
+    $position = $_POST['posFeed'];
+    $title = $_POST['title'];
+    $image = $_POST['image'];
+    $publisher = $_POST['publisher'];
+    $body = $_POST['body'];
+    $source = $_POST['source'];
+
+    $feedController=new feedController();
+
+    $feedArray = $feedController->updateFeed($title,$image,$publisher,$body,$source,$position,$_SESSION['feed']);
+
+    $_SESSION['feed']=$feedArray;
+
+    header('Location:../../../index.php');
+
+?>
