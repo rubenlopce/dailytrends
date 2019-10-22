84f62505b4f8a6231759c75b03405c270a32eff0
rubenlopce <rubenlopce@gmail.com>
2019-10-22 09:48:11 +0200

    Updated list.php and index.php files.

--- config/views/feed/list.php
+++ config/views/feed/list.php
@@ -1,10 +1,30 @@
-<div>
+<article class='feedContainer__box'>
 
-    <img src="<?=$feed->getImage();?>" alt="">
-    <h1><?=$feed->getTitle();?></h1>
-    <p><?=$feed->getPublisher();?></p>
-    <p><?=$feed->getBody();?></p>
-    <small><?=$feed->getSource();?></small>
-    <button class='details' value='<?= $key ?>'>Detalles</button>
+    <div class="feedContainer__box__img" style="background:url(<?=$feed->getImage();?>);"></div>
+    
+    <div class="feedContainer__box__body">
 
-</div>
+        <div class="feedContainer__box__body__title">
+
+            <h3><?=$feed->getTitle();?></h3>
+            <hr>
+            <small class="feedContainer__box__body__title_publisher"><?=$feed->getPublisher();?></small>
+            
+        </div>
+        
+        <div class="feedContainer__box__body__description">
+
+            <p><?=$feed->getBody();?></p>
+            <small><?=$feed->getSource();?></small>
+            
+        </div>
+
+        <div class="feedContainer__box__body__info">
+
+            <button value='<?= $key ?>' class="feedContainer__box__body__info__details">Detalles</button>
+
+        </div>
+       
+    </div>
+
+</article>
